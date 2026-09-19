a899573f3927e62dd36bef8a9ddb7759ecfe1b92
Parth Prashant Pate <parth.p.pate07@gmail.com>
2026-09-20 01:04:56 +0530

    Bump versionName to 2.0.1 (v2.0.1 patch release)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.example.watchorderengine"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "2.0.0"
+        versionCode = 4
+        versionName = "2.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
